23795b2f2a34ebc8b7451f437e390dc8dc9064c0
ALEXTANG <574809918@qq.com>
2021-09-23 19:59:40 +0800

    Init

---
+++ src/views/About.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+
+    <div>
+      <b-navbar toggleable="lg" type="dark" variant="info">
+        <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item href="#">Link</b-nav-item>
+            <b-nav-item href="#" disabled>Disabled</b-nav-item>
+          </b-navbar-nav>
+
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-nav-form>
+              <b-form-input
+                size="sm"
+                class="mr-sm-2"
+                placeholder="Search"
+              ></b-form-input>
+              <b-button size="sm" class="my-2 my-sm-0" type="submit"
+                >Search</b-button
+              >
+            </b-nav-form>
+
+            <b-nav-item-dropdown text="Lang" right>
+              <b-dropdown-item href="#">EN</b-dropdown-item>
+              <b-dropdown-item href="#">ES</b-dropdown-item>
+              <b-dropdown-item href="#">RU</b-dropdown-item>
+              <b-dropdown-item href="#">FA</b-dropdown-item>
+            </b-nav-item-dropdown>
+
+            <b-nav-item-dropdown right>
+              <!-- Using 'button-content' slot -->
+              <template v-slot:button-content>
+                <em>User</em>
+              </template>
+              <b-dropdown-item href="#">Profile</b-dropdown-item>
+              <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
+
+    <div>
+      <b-carousel
+        id="carousel-no-animation"
+        style="text-shadow: 0px 0px 2px #000"
+        no-animation
+        indicators
+        img-width="1024"
+        img-height="480"
+      >
+        <b-carousel-slide
+          caption="First slide"
+          img-src="https://picsum.photos/1024/480/?image=10"
+        ></b-carousel-slide>
+        <b-carousel-slide
+          caption="Second Slide"
+          img-src="https://picsum.photos/1024/480/?image=12"
+        ></b-carousel-slide>
+        <b-carousel-slide
+          caption="Third Slide"
+          img-src="https://picsum.photos/1024/480/?image=22"
+        ></b-carousel-slide>
+        <b-carousel-slide
+          caption="Fourth Slide"
+          img-src="https://picsum.photos/1024/480/?image=23"
+        ></b-carousel-slide>
+      </b-carousel>
+    </div>
+
+    <b-row>
+      <b-col md="auto">
+        <b-calendar
+          v-model="value"
+          @context="onContext"
+          locale="en-US"
+        ></b-calendar>
+      </b-col>
+      <!-- <b-col>
+      <p>Value: <b>'{{ value }}'</b></p>
+      <p class="mb-0">Context:</p>
+      <pre class="small">{{ context }}</pre>
+    </b-col> -->
+    </b-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: "",
+      context: null,
+    };
+  },
+  methods: {
+    onContext(ctx) {
+      this.context = ctx;
+    },
+  },
+};
+</script>
---
+++ src/views/login/Login.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="login">
+    <b-row class="mt-5">
+      <b-col
+        md="8"
+        offset-md="2"
+      >
+        <b-card title="登录">
+          <b-form>
+            <b-form-group label="账号">
+              <b-form-input
+                v-model="user.name"
+                type="text"
+                placeholder="Enter name"
+              ></b-form-input>
+            </b-form-group>
+            <b-form-group label="手机">
+              <b-form-input
+                v-model="user.telephone"
+                type="number"
+                placeholder="Enter telephone"
+              ></b-form-input>
+
+              <b-form-text
+                id="password-help-block"
+                text-variant="danger"
+                v-if="ShowTelephoneDanger"
+              >
+                手机号的长度必须为11位
+              </b-form-text>
+            </b-form-group>
+            <b-form-group label="密码">
+              <b-form-input
+                v-model="user.password"
+                type="password"
+                placeholder="Enter password"
+              ></b-form-input>
+            </b-form-group>
+            <b-form-group>
+              <b-button
+                variant="primary"
+                block
+                @click="login"
+              >登录</b-button>
+            </b-form-group>
+          </b-form>
+        </b-card>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        name: "",
+        telephone: "",
+        password: ""
+      },
+      ShowTelephoneDanger: false
+    };
+  },
+  methods: {
+    register() {
+      if (this.user.telephone.length != 11) {
+        this.ShowTelephoneDanger = true;
+      }
+      console.log("register");
+    },
+    login() {
+      if (this.user.telephone.length != 11) {
+        this.ShowTelephoneDanger = true;
+      }
+      console.log("login");
+    }
+  }
+};
+</script>
+
+<style  lang="scss">
+</style>
